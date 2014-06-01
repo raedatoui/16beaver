a08a310daea7d639a1708f1edc43be215ea36d7e
Raed Atoui <raed.atoui@gmail.com>
2014-06-01 00:38:35 -0400

    keep PT sans

--- header.php
+++ header.php
@@ -36,12 +36,11 @@
       else
         $color_class = "light-gray";
     }
-
   ?>
 
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-  <?php
 
+  <?php
     remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
     remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
     remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
@@ -53,7 +52,8 @@
     remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
 
     wp_deregister_script('jquery');
-    wp_head(); ?>
+    wp_head();
+  ?>
 
 </head>
 
